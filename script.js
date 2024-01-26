ebd3a27306f3ee8791f29d00c47d0dc83dfde8e2
Trúc Mai <mtruc7097@gmail.com>
2024-01-27 00:38:27 +0700

    abc

---
+++ script.js
@@ -0,0 +1,14 @@
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.get('/', (req, res) => { //=>: around function; / định nghĩa tuyến đường
+  res.send('Hello world')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port http://localhost:${port}`)
+})
+function alertTest() {
+    alert("OK")
+}
