8e306900b7dbb10610fdd5aaf4f6663333735afc
Trúc Mai <mtruc7097@gmail.com>
2024-02-01 23:14:01 +0700

    ely

--- src/script.js
+++ src/script.js
@@ -5,17 +5,20 @@ const handlebars = require('express-handlebars')
 const app = express()
 const port = 3000
 
+
 // http logger
 app.use(morgan('combined'))
 
 // template engine
-app.engine('handlebars', handlebars());
-app.set('view engine', 'handlebars');
-app.set('views',path.join(_dirname, 'src/rerources/views'));
+app.engine('hbs', handlebars.engine({extname: '.hbs' }))
+app.set('view engine', 'hbs')
+app.set('views',path.join(__dirname, 'resources/views'))
+
 
 app.get('/', (req, res) => { //=>: around function; / định nghĩa tuyến đường
-  res.render('Home')
+  res.render('home')
 })
+
 
 app.listen(port, () => {
   console.log(`Example app listening on port http://localhost:${port}`)
