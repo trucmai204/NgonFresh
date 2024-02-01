4806ea5d7476971c63476e89938eb76fea20c365
Trúc Mai <mtruc7097@gmail.com>
2024-02-01 12:45:59 +0700

    PP

--- script.js
+++ script.js
@@ -1,9 +1,20 @@
+const path = require('path')
 const express = require('express')
+const morgan = require('morgan')
+const handlebars = require('express-handlebars')
 const app = express()
 const port = 3000
 
-app.get('/tin-tuc', (req, res) => { //=>: around function; / định nghĩa tuyến đường
-  res.send('Hello world')
+// http logger
+app.use(morgan('combined'))
+
+// template engine
+app.engine('handlebars', handlebars());
+app.set('view engine', 'handlebars');
+app.set('views',path.join(_dirname, 'src/rerources/views'));
+
+app.get('/', (req, res) => { //=>: around function; / định nghĩa tuyến đường
+  res.render('Home')
 })
 
 app.listen(port, () => {
