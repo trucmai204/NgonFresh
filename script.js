b343791a9f6a7e6ebc757c1fd6d26479d0012357
Trúc Mai <mtruc7097@gmail.com>
2024-02-05 22:46:58 +0700

    toi loi

--- script.js
+++ script.js
@@ -1,7 +1,6 @@
 const path = require('path')
 const express = require('express')
 const morgan = require('morgan')
-const handlebars = require('express-handlebars')
 const app = express()
 const port = 3000
 
@@ -9,16 +8,13 @@ const port = 3000
 // http logger
 app.use(morgan('combined'))
 
-// template engine
-app.engine('hbs', handlebars.engine({extname: '.hbs'}))
-app.set('view engine', 'hbs')
-app.set('views',path.join(__dirname, 'resources/views'))
-
+app.use(express.static('public'));
 
 app.get('/', (req, res) => { //=>: around function; / định nghĩa tuyến đường
-  res.render('home')
-})
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
 
+ 
 
 app.listen(port, () => {
   console.log(`Example app listening on port http://localhost:${port}`)
