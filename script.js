d8014199f656bb6ca8d7485f32db2274cc54b89b
Trúc Mai <mtruc7097@gmail.com>
2024-02-08 22:30:49 +0700

    acd

--- script.js
+++ script.js
@@ -64,9 +64,28 @@ app.post('/login', (req, res) => {
   // Xử lý đăng nhập ở đây
 
   // Sau khi xử lý đăng nhập thành công, chuyển hướng người dùng đến trang index
-  res.redirect('/public/index');
+  res.redirect('/index.html');
 });
+app.post('/register', (req, res) => {
+  const { username, password, email } = req.body;
 
+  // Thực hiện truy vấn SQL INSERT để thêm người dùng mới vào cơ sở dữ liệu
+  connection.query(
+    'INSERT INTO khachhang (TenKH, MK, Email) VALUES (?, ?, ?)',
+    [username, password, email],
+    (err, results) => {
+      if (err) {
+        console.error('Error inserting data into database:', err);
+        res.status(500).json({ error: 'Error inserting data into database' });
+        return;
+      }
+      console.log('Inserted new record into users table:', results);
+      
+      // Chuyển hướng người dùng đến trang index sau khi đăng ký thành công
+      res.sendFile(path.join(__dirname, 'index.html'));
+    }
+  );
+});
 
 app.listen(port, () => {
   console.log(`Example app listening on port http://localhost:${port}`)
